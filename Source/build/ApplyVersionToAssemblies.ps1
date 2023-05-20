a4b5649de5fdb4981726136f20815e107a5f6f8b
LaunchGemini <metabaseguy@proton.me>
2023-05-20 19:27:44 +0000

    new flies

--- Source/build/ApplyVersionToAssemblies.ps1
+++ Source/build/ApplyVersionToAssemblies.ps1
@@ -4,4 +4,36 @@
 # Look for a 0.0.0.0 pattern in the build number. 
 # If found use it to version the assemblies.
 #
-# For example, if the 'Build number format' build process pa
+# For example, if the 'Build number format' build process parameter 
+# $(BuildDefinitionName)_$(Year:yyyy).$(Month).$(DayOfMonth)$(Rev:.r)
+# then your build numbers come out like this:
+# "Build HelloWorld_2013.07.19.1"
+# This script would then apply version 2013.07.19.1 to your assemblies.
+	
+# Enable -Verbose option
+[CmdletBinding()]
+	
+# Disable parameter
+# Convenience option so you can debug this script or disable it in 
+# your build definition without having to remove it from
+# the 'Post-build script path' build process parameter.
+param([switch]$Disable)
+if ($PSBoundParameters.ContainsKey('Disable'))
+{
+	Write-Verbose "Script disabled; no actions will be taken on the files."
+}
+	
+# Regular expression pattern to find the version in the build number 
+# and then apply it to the assemblies
+$VersionRegex = "\d+\.\d+\.\d+\.\d+"
+	
+# If this script is not running on a build server, remind user to 
+# set environment variables so that this script can be debugged
+if(-not $Env:BUILD -and -not ($Env:BUILD_SOURCESDIRECTORY -and $Env:BUILD_BUILDID))
+{
+	Write-Error "You must set the following environment variables"
+	Write-Error "to test this script interactively."
+	Write-Host '$Env:BUILD_SOURCESDIRECTORY - For example, enter something like:'
+	Write-Host '$Env:BUILD_SOURCESDIRECTORY = "C:\code\FabrikamTFVC\HelloWorld"'
+	Write-Host '$Env:BUILD_BUILDNUMBER - For example, enter something like:'
+	Write-Host '$Env:BUILD_BUILD
