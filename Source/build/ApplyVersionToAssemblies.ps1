a29907e1eddc9038721c00f6998a1e540acbed67
LaunchGemini <metabaseguy@proton.me>
2023-05-20 13:21:15 +0000

    add background

---
+++ Source/build/ApplyVersionToAssemblies.ps1
@@ -0,0 +1,7 @@
+##-----------------------------------------------------------------------
+## <copyright file="ApplyVersionToAssemblies.ps1">(c) http://TfsBuildExtensions.codeplex.com/. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+##-----------------------------------------------------------------------
+# Look for a 0.0.0.0 pattern in the build number. 
+# If found use it to version the assemblies.
+#
+# For example, if the 'Build number format' build process pa
