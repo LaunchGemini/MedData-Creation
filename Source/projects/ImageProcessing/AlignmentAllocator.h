52e9c6f12cdacc4f0936fa591859313c6a0ccbc5
LaunchGemini <metabaseguy@proton.me>
2023-05-23 06:38:16 +0000

    security updates

--- Source/projects/ImageProcessing/AlignmentAllocator.h
+++ Source/projects/ImageProcessing/AlignmentAllocator.h
@@ -22,4 +22,55 @@ namespace createdataset
     typedef const T & const_reference;
 
   public:
-    inline AlignmentAllocator() throw () {
+    inline AlignmentAllocator() throw () { }
+
+    template <typename T2>
+    inline AlignmentAllocator(const AlignmentAllocator<T2, N> &) throw () { }
+
+    inline ~AlignmentAllocator() throw () { }
+
+    inline pointer adress(reference r) {
+      return &r;
+    }
+
+    inline const_pointer adress(const_reference r) const {
+      return &r;
+    }
+
+    inline pointer allocate(size_type n) {
+      return (pointer)_aligned_malloc(n*sizeof(value_type), N);
+    }
+
+    inline void deallocate(pointer p, size_type) {
+      _aligned_free(p);
+    }
+
+    inline void construct(pointer p, const value_type & wert) {
+      new (p) value_type(wert);
+    }
+
+    inline void destroy(pointer p) {
+      p->~value_type();
+    }
+
+    inline size_type max_size() const throw () {
+      return size_type(-1) / sizeof(value_type);
+    }
+
+    template <typename T2>
+    struct rebind {
+      typedef AlignmentAllocator<T2, N> other;
+    };
+
+    bool operator!=(const AlignmentAllocator<T, N>& other) const {
+      return !(*this == other);
+    }
+
+    // Returns true if and only if storage allocated from *this
+    // can be deallocated from other, and vice versa.
+    // Always returns true for stateless allocators.
+    bool operator==(const AlignmentAllocator<T, N>& other) const {
+      return true;
+    }
+  };
+}
