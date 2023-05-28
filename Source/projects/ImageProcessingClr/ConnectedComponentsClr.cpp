add805f51b46bc76952648f45ec264b6e6b720a7
LaunchGemini <metabaseguy@proton.me>
2023-05-28 15:17:57 +0000

    Use Docker at Travis

--- Source/projects/ImageProcessingClr/ConnectedComponentsClr.cpp
+++ Source/projects/ImageProcessingClr/ConnectedComponentsClr.cpp
@@ -12,4 +12,38 @@
 #include "connectedComponents.h"
 #pragma managed(pop)
 
-namespace InnerEye 
+namespace InnerEye {  namespace CreateDataset { namespace ImageProcessing {
+      int ConnectedComponents::Find3d(
+        array<unsigned char>^ image,
+        int width, int height, int depth,
+        unsigned char backgroundColour,
+        array<unsigned short>^ output)
+      {
+        try
+        {
+          pin_ptr<unsigned char> inputBuffer = &image[0];
+          pin_ptr<unsigned short> outputBuffer = &output[0];
+
+          int inputLeap = width*height*sizeof(unsigned char), inputStride = width*sizeof(unsigned char);
+          int outputLeap = width*height*sizeof(unsigned short), outputStride = width*sizeof(unsigned short);
+
+          auto result = createdataset::findConnectedComponents3d<unsigned char, unsigned short>(
+            width, height, depth,
+            inputBuffer, inputLeap, inputStride, backgroundColour,
+            outputBuffer, outputLeap, outputStride,
+            0);
+
+          return (int)(result.size());
+        }
+        catch (std::exception& oops)
+        {
+          throw gcnew System::Exception(gcnew System::String(oops.what()));
+        }
+      }
+
+      array<ComponentStatistics>^ ConnectedComponents::Find3dWithStatistics(
+        array<unsigned char>^ image,
+        int width, int height, int depth,
+        unsigned char backgroundColour,
+        array<unsigned short>^ output)
+      
