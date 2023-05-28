67ae6a3594995d7f9b0a12bf21e9b2eb29f3add5
LaunchGemini <metabaseguy@proton.me>
2023-05-28 17:32:51 +0000

    Applied updates

--- Source/projects/ImageProcessingClr/ConnectedComponentsClr.cpp
+++ Source/projects/ImageProcessingClr/ConnectedComponentsClr.cpp
@@ -46,4 +46,30 @@ namespace InnerEye {  namespace CreateDataset { namespace ImageProcessing {
         int width, int height, int depth,
         unsigned char backgroundColour,
         array<unsigned short>^ output)
-      
+      {
+        try
+        {
+          pin_ptr<unsigned char> inputBuffer = &image[0];
+          pin_ptr<unsigned short> outputBuffer = &output[0];
+
+          int inputLeap = width*height*sizeof(unsigned char), inputStride = width*sizeof(unsigned char);
+          int outputLeap = width*height*sizeof(unsigned short), outputStride = width*sizeof(unsigned short);
+
+          auto result_ = createdataset::findConnectedComponents3d<unsigned char, unsigned short>(
+            width, height, depth,
+            inputBuffer, inputLeap, inputStride, backgroundColour,
+            outputBuffer, outputLeap, outputStride,
+            0);
+
+          auto result = gcnew array<ComponentStatistics>(result_.size());
+          pin_ptr<ComponentStatistics> p = &result[0];
+          ::memcpy(p, &result_[0], result_.size()*sizeof(createdataset::ComponentStatistics<unsigned char,unsigned short>));
+
+          return result;
+        }
+        catch (std::exception& oops)
+        {
+          throw gcnew System::Exception(gcnew System::String(oops.what()));
+        }
+      }
+} } }
