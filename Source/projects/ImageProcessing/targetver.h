074c9f63c75c775caf527f68d46c9f86147e1683
LaunchGemini <metabaseguy@proton.me>
2023-05-28 13:24:06 +0000

    changes log

--- Source/projects/ImageProcessing/targetver.h
+++ Source/projects/ImageProcessing/targetver.h
@@ -4,17 +4,11 @@
  *  ------------------------------------------------------------------------------------------
  */
 
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
-#include "targetver.h"
+// Including SDKDDKVer.h defines the highest available Windows platform.
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
-
-
-// TODO: reference additional headers your program requires here
+#include <SDKDDKVer.h>
--- Source/projects/ImageProcessingClr/ConnectedComponentsClr.cpp
+++ Source/projects/ImageProcessingClr/ConnectedComponentsClr.cpp
@@ -4,17 +4,12 @@
  *  ------------------------------------------------------------------------------------------
  */
 
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+#include "ConnectedComponentsClr.h"
 
-#pragma once
+#include <cliext/vector>
 
-#include "targetver.h"
+#pragma managed(push, off)
+#include "connectedComponents.h"
+#pragma managed(pop)
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-
-
-
-// TODO: reference additional headers your program requires here
+namespace InnerEye 
