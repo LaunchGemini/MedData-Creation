460368fd2386112ff362661883bb37cb4fe08fb5
LaunchGemini <metabaseguy@proton.me>
2023-06-04 19:18:54 +0000

    updated package.json

--- Source/projects/ImageProcessingClr/ConvolutionClr.h
+++ Source/projects/ImageProcessingClr/ConvolutionClr.h
@@ -19,4 +19,12 @@ namespace InnerEye { namespace CreateDataset { namespace ImageProcessing {
     // TODO: Various improvements are possible:
     // * Take a ROI as argument to avoid the need to extract subregion from managed array.
     // * Support out-of-place convolution too?
-    // * Suppor
+    // * Support arbtirary kernel in array.
+
+    static void Convolve(array<float>^ data, int width, int height, int depth, array<Direction>^ directions, array<float>^ sigmas);
+
+    static void Convolve(array<unsigned char>^ data, int width, int height, int depth, array<Direction>^ directions, array<float>^ sigmas);
+
+    static void Convolve(array<short>^ data, int width, int height, int depth, array<Direction>^ directions, array<float>^ sigmas);
+  };
+} } }
