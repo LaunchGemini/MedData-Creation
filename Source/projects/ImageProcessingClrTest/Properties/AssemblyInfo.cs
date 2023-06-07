6c4394cefbcdbd51e564bf6f8425e41c0fa2f1b5
LaunchGemini <metabaseguy@proton.me>
2023-06-07 14:36:31 +0000

    working progress

--- Source/projects/ImageProcessingClrTest/Properties/AssemblyInfo.cs
+++ Source/projects/ImageProcessingClrTest/Properties/AssemblyInfo.cs
@@ -1,1 +1,33 @@
-///  ---------------------------------------------------------------------------------------
+///  ------------------------------------------------------------------------------------------
+///  Copyright (c) Microsoft Corporation. All rights reserved.
+///  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+///  ------------------------------------------------------------------------------------------
+
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageProcessingClrTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageProcessingClrTest")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7604598-10f9-41cc-bbf4-805c67f48263")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//     
