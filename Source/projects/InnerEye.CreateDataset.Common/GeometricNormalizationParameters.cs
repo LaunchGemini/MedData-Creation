8c7087f855a98a8be448648f8ee64d7db96d272e
LaunchGemini <metabaseguy@proton.me>
2023-06-10 13:24:13 +0000

    Resolved merge conflicts

--- Source/projects/InnerEye.CreateDataset.Common/GeometricNormalizationParameters.cs
+++ Source/projects/InnerEye.CreateDataset.Common/GeometricNormalizationParameters.cs
@@ -11,4 +11,40 @@ using System.Linq;
 namespace InnerEye.CreateDataset.Common.Models
 {
     /// <summary>
-    /// Parameters class to encapsulate the configurations required to perform Geometric No
+    /// Parameters class to encapsulate the configurations required to perform Geometric Normalization operations
+    /// </summary>
+    public class GeometricNormalizationParameters
+    {
+        /// <summary>
+        /// Enumeration to encapsulate the differnt kinds of channels that can be provided as input for Geometric Normalization
+        /// </summary>
+        public enum GeometricNormalizationChannelType
+        {
+            /// <summary>
+            /// Specifies that the channel contains an image, as short volume.
+            /// </summary>
+            Image,
+            /// <summary>
+            /// Specifies that the channel contains a ground truth segmentation, as a byte volume.
+            /// </summary>
+            GroundTruth
+        }
+
+        /// <summary>
+        /// Wrapper class to encapsulate a single input-output-chanel type config
+        /// </summary>
+        public class InputOutputChannel
+        {
+            [JsonRequired]
+            public string InputChannelId { get; set; }
+
+            [JsonRequired]
+            public string OutputChannelId { get; set; }
+
+            [JsonRequired]
+            public GeometricNormalizationChannelType ChannelType { get; set; }
+
+            public InputOutputChannel(string inputChannelId, string outputChannelId, GeometricNormalizationChannelType channelType)
+            {
+                InputChannelId = inputChannelId;
+                OutputChannelId = output
