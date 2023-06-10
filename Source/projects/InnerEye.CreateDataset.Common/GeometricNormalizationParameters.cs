0aca98024c2ccc77d62323c384b7c93890298676
LaunchGemini <metabaseguy@proton.me>
2023-06-10 12:41:38 +0000

    Typo's

---
+++ Source/projects/InnerEye.CreateDataset.Common/GeometricNormalizationParameters.cs
@@ -0,0 +1,14 @@
+///  ------------------------------------------------------------------------------------------
+///  Copyright (c) Microsoft Corporation. All rights reserved.
+///  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+///  ------------------------------------------------------------------------------------------
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InnerEye.CreateDataset.Common.Models
+{
+    /// <summary>
+    /// Parameters class to encapsulate the configurations required to perform Geometric No
