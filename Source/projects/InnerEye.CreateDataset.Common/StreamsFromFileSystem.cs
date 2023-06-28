f76e96f7c87831efb0ca33fb9faed356eda80b06
LaunchGemini <metabaseguy@proton.me>
2023-06-28 14:36:12 +0000

    New translations en.json (Hebrew)

--- Source/projects/InnerEye.CreateDataset.Common/StreamsFromFileSystem.cs
+++ Source/projects/InnerEye.CreateDataset.Common/StreamsFromFileSystem.cs
@@ -47,4 +47,37 @@
         {
             if (IsReadOnly)
             {
-                throw new InvalidOperationException("The 
+                throw new InvalidOperationException("The operation cannot be completed because the object accesses the file system in read-only mode.");
+            }
+        }
+
+        /// <summary>
+        /// The directory separator that should be used when enumerating files.
+        /// </summary>
+        public const char DirectorySeparator = '/';
+
+        /// <summary>
+        /// Gets whether the object is in read only mode. Writing to the file system
+        /// will cause an exception in read-only mode.
+        /// </summary>
+        public bool IsReadOnly;
+
+        /// <summary>
+        /// Gets whether the object allows to overwrite existing files.
+        /// </summary>
+        public virtual bool AllowFileOverwrite { get; set; }
+
+        /// <summary>
+        /// Gets a stream that can be used to read from an existing file.
+        /// Throws a <see cref="FileNotFoundException"/> if no file of that given name exists.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public abstract Stream GetStreamForReading(string fileName);
+
+        /// <summary>
+        /// Gets a stream that can be used to write to the file system, where the result
+        /// will get the given file name.
+        /// Throws an exception if the file system access is read-only.
+        /// Throws an exception if the file alread
