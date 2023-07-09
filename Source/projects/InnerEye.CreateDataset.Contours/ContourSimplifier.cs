1917511e1e6261305f12fac6f966c7c163c322da
LaunchGemini <metabaseguy@proton.me>
2023-07-09 19:24:44 +0000

    name changed

--- Source/projects/InnerEye.CreateDataset.Contours/ContourSimplifier.cs
+++ Source/projects/InnerEye.CreateDataset.Contours/ContourSimplifier.cs
@@ -14,4 +14,29 @@
 
     /// <summary>
     /// Class to simplify contours induced by segmentation masks defined on a raster grid.
-    /// Such contours can be described by sequences of one-unit-long moves with
+    /// Such contours can be described by sequences of one-unit-long moves with direction
+    /// expressed relative to the preceding move. For illustration, consider the following open
+    /// contour:
+    /// x0
+    /// |
+    /// x1
+    /// |
+    /// x2--x3--x4
+    ///          |
+    ///          x5
+    /// This contour may be defined by 6 vertices x0, x1, etc and 5 edges. Equivalently
+    /// the contour may be defined using a starting position (x0) and starting direction
+    /// (down the page) and a sequence of moves (in {Forwards, Left, Right}:
+    /// FFLFR
+    /// The appeal of this representation is invariance to local orientation. This motivates
+    /// the contour simplification approach used here, which works by replacing patterns of moves
+    /// described by sequences of characters in a string (e.g. LRL) with more complex paths defined
+    /// (in a direction-relative way) in the continuous 2D domain.
+    /// </summary>
+    public static class ContourSimplifier
+    {
+        private static readonly Tuple<string, PointF[]>[] PatternToFragmentMap = new Tuple<string, PointF[]>[]
+        {
+            // NB These fragments are defined in order of decreasing priorty.
+            Tuple.Create("FRF", new PointF[] { new PointF(0, -0.5f), new PointF(0, 0.1f), new PointF(-0.9f, 1), new PointF(-1.5f, 1) }),
+            Tuple.Create("FLF", new PointF[] { new PointF(0, -0.5f), new PointF(0, 0.1f), new PointF(0.9f, 1), new P
