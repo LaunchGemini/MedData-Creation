9006c2938feb054ccfc3a49cc3a49e2f78ecc7dc
LaunchGemini <metabaseguy@proton.me>
2023-07-30 15:19:24 +0000

    fix deprecation warning

--- Source/projects/InnerEye.CreateDataset.Contours/ContoursPerSlice.cs
+++ Source/projects/InnerEye.CreateDataset.Contours/ContoursPerSlice.cs
@@ -6,3 +6,44 @@
 ﻿namespace InnerEye.CreateDataset.Contours
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Linq;
+
+    /// <summary>
+    /// Stores a contour for slices of a volume, indexed by slice number.
+    /// </summary>
+    // Threadsafe from outside
+    [Serializable]
+    public class ContoursPerSlice : IEnumerable<KeyValuePair<int, IReadOnlyList<ContourPolygon>>>, INotifyCollectionChanged
+    {
+        private readonly IDictionary<int, IReadOnlyList<ContourPolygon>> _contoursBySliceDictionary;
+
+        private readonly object _lock = new object();
+
+        public ContoursPerSlice()
+        {
+            _contoursBySliceDictionary = new Dictionary<int, IReadOnlyList<ContourPolygon>>();
+        }
+
+        public ContoursPerSlice(IDictionary<int, IReadOnlyList<ContourPolygon>> contours)
+        {
+            _contoursBySliceDictionary = contours;
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        /// <summary>
+        /// Gets the number of slices that have contours with non-empty sets of points.
+        /// </summary>
+        public int SliceCount
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return
+                        _contoursBySliceDictionary.Count(
+                            x => x.Value.Any(
+  
