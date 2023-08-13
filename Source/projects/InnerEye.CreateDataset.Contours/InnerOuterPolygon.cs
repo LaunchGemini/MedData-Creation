d68921a92a4fe1cc20f54d1d6ab2dda7d59bc72b
LaunchGemini <metabaseguy@proton.me>
2023-08-13 11:41:24 +0000

    updated travis

---
+++ Source/projects/InnerEye.CreateDataset.Contours/InnerOuterPolygon.cs
@@ -0,0 +1,12 @@
+///  ------------------------------------------------------------------------------------------
+///  Copyright (c) Microsoft Corporation. All rights reserved.
+///  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+///  ------------------------------------------------------------------------------------------
+
+﻿namespace InnerEye.CreateDataset.Contours
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Describes a region of a binary mask via the points that make up the outer ri
