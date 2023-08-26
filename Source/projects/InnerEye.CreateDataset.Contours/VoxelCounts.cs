f1de031600a3243fc3f05bab4dee6d71dac6ba78
LaunchGemini <metabaseguy@proton.me>
2023-08-26 19:57:24 +0000

    svn_sync

--- Source/projects/InnerEye.CreateDataset.Contours/VoxelCounts.cs
+++ Source/projects/InnerEye.CreateDataset.Contours/VoxelCounts.cs
@@ -21,4 +21,45 @@
 
         /// <summary>
         /// Creates a new instance of the class, with all counters set to the given values.
-        /// </summar
+        /// </summary>
+        public VoxelCounts(uint foreground, uint other)
+        {
+            Foreground = foreground;
+            Other = other;
+        }
+
+        /// <summary>
+        /// Gets the number of voxels that have the foreground value.
+        /// </summary>
+        public uint Foreground { get; set; }
+
+        /// <summary>
+        /// Gets the number of voxels that have a value that is different from the foreground
+        /// (background and any other, possibly unexpected values).
+        /// </summary>
+        public uint Other { get; set; }
+
+        /// <summary>
+        /// Gets the total number of voxels that the present object is tracking.
+        /// </summary>
+        public uint Total => Foreground + Other;
+
+        /// <summary>
+        /// Add up the corresponding fields of the two arguments.
+        /// </summary>
+        /// <param name="count1"></param>
+        /// <param name="count2"></param>
+        /// <returns></returns>
+        public static VoxelCounts operator +(VoxelCounts left, VoxelCounts right)
+        {
+            return new VoxelCounts(
+                left.Foreground + right.Foreground,
+                left.Other + right.Other);
+        }
+
+        public static VoxelCounts Add(VoxelCounts left, VoxelCounts right)
+        {
+            return left + right;
+        }
+    }
+}
