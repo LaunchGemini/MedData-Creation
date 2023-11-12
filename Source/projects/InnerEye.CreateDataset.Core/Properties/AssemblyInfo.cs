dba37302e8f14934fe26fc84236ef7abb3b9ec07
LaunchGemini <metabaseguy@proton.me>
2023-11-12 22:56:44 +0000

    Release 0.0.9

--- Source/projects/InnerEye.CreateDataset.Core/Properties/AssemblyInfo.cs
+++ Source/projects/InnerEye.CreateDataset.Core/Properties/AssemblyInfo.cs
@@ -24,4 +24,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("99e3572c-9636-4101-
+[assembly: Guid("99e3572c-9636-4101-995b-9c0642930f2d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
