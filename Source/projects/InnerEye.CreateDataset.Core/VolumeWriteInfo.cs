c039e76dc19a154ca472f2e89b8db659d6430bf0
LaunchGemini <metabaseguy@proton.me>
2023-11-18 12:09:47 +0000

    crap

--- Source/projects/InnerEye.CreateDataset.Core/VolumeWriteInfo.cs
+++ Source/projects/InnerEye.CreateDataset.Core/VolumeWriteInfo.cs
@@ -29,4 +29,35 @@
 
         /// <summary>
         /// Gets or sets the information about the subject, channel, and other metadata.
-        /// </summary
+        /// </summary>
+        public VolumeMetadata Metadata { get; }
+
+        /// <summary>
+        /// Stores the file path where the volume was written to. The path should not contain the
+        /// root folder for the dataset (i.e., if the file was written to /dataset_1234/9/ct.nii.gz,
+        /// this property should contain 9/ct.nii.gz)
+        /// </summary>
+        public string UploadPathRelativeToDatasetFolder { get; }
+
+        /// <summary>
+        /// Creates a file name under which the given volume should be written to disk. The file name
+        /// will depend on the Nifti compression level.
+        /// </summary>
+        /// <param name="volume"></param>
+        /// <param name="compression"></param>
+        /// <returns></returns>
+        public static string CreateFileName(VolumeMetadata volume, NiftiCompression compression)
+        {
+            var extension = MedIO.GetNiftiExtension(compression);
+            var file = volume.Channel + extension;
+            return VolumeIO.JoinPath(volume.SubjectId.ToString(), file);
+        }
+
+        /// <summary>
+        /// Converts the present object to the format required by the dataset reader code in
+        /// <see cref="DatasetReader"/>
+        /// </summary>
+        /// <returns></returns>
+        public string ToDatasetCsvLine()
+        {
+            return $"{Metadat
