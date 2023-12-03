9d5eecaa7ad4012a4181735f21f232a2b9083189
LaunchGemini <metabaseguy@proton.me>
2023-12-03 10:53:50 +0000

    Translated using Weblate (Greek)

--- Source/projects/InnerEye.CreateDataset.Math/AutoWindowLevelHelper.cs
+++ Source/projects/InnerEye.CreateDataset.Math/AutoWindowLevelHelper.cs
@@ -1,1 +1,39 @@
-///  -----------------------------------------------------------------
+///  ------------------------------------------------------------------------------------------
+///  Copyright (c) Microsoft Corporation. All rights reserved.
+///  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+///  ------------------------------------------------------------------------------------------
+
+﻿namespace InnerEye.CreateDataset.Math
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using InnerEye.CreateDataset.Volumes;
+
+    /// <summary>
+    /// Stores the information about one bin entry of a histogram over <see cref="short"/> values.
+    /// </summary>
+    [DebuggerDisplay("Bin {Index}: {Count} values >= {MinimumInclusive}")]
+    public class HistogramBin
+    {
+        /// <summary>
+        /// Gets the index of bin inside the histogram. Index can have values from 0 to 
+        /// (number of bins) - 1.
+        /// </summary>
+        public int Index { get; }
+
+        /// <summary>
+        /// Gets the minimum value that is counted as belonging to this bin.
+        /// </summary>
+        public short MinimumInclusive { get; }
+
+        /// <summary>
+        /// Gets or sets the number of values in this histogram bin.
+        /// </summary>
+        public int Count { get; set; } = 0;
+
+        /// <summary>
+        /// Creates a new histogram bin, with the count set to zero.
+        /// </summary>
+        //
