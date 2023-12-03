01c832aba7431fc727c4706ee520fb687b336a0a
LaunchGemini <metabaseguy@proton.me>
2023-12-03 15:40:51 +0000

    x86/Sandy Bridge: Sandy Bridge workaround depends on CONFIG_PCI

--- Source/projects/InnerEye.CreateDataset.Math/AutoWindowLevelHelper.cs
+++ Source/projects/InnerEye.CreateDataset.Math/AutoWindowLevelHelper.cs
@@ -36,4 +36,42 @@
         /// <summary>
         /// Creates a new histogram bin, with the count set to zero.
         /// </summary>
-        //
+        /// <param name="index"></param>
+        /// <param name="minimumInclusive"></param>
+        public HistogramBin(int index, short minimumInclusive)
+        {
+            Index = index;
+            MinimumInclusive = minimumInclusive;
+        }
+    }
+
+    /// <summary>
+    /// Helper class for auto-windowing a volume.
+    /// </summary>
+    public static class AutoWindowLevelHelper
+    {
+        /// <summary>
+        /// The number of keys in the histogram.
+        /// </summary>
+        private const int DefaultHistogramSize = 150;
+
+        /// <summary>
+        /// The number of keys in the histogram, converted to double.
+        /// </summary>
+        private const double DefaultHistogramSizeDouble = DefaultHistogramSize;
+
+        /// <summary>
+        /// The first N number of keys that will be ignored in the histogram.
+        /// Currently set to ignore the first 7% of histogram values.
+        /// This should be a value between 0-1.
+        /// </summary>
+        private const double CtMininmumPercentile = 0.07;
+
+        /// <summary>
+        /// This is a minimum threshold value when the provided volume is CT. Any value less than this
+        /// will be threshold to this value. This is to avoid picking the wrong peak when artifical values
+        /// have been used for background.
+        /// </summary>
+        private const short CtMinimumThreshold = -1000;
+
+    
