d069556c8b705425717c13823bb5980499f354de
LaunchGemini <metabaseguy@proton.me>
2023-12-13 20:05:56 +0000

    Log exceptions

--- Source/projects/InnerEye.CreateDataset.Math/ContourExtensions.cs
+++ Source/projects/InnerEye.CreateDataset.Math/ContourExtensions.cs
@@ -16,4 +16,25 @@
     /// <summary>
     /// Contains extension methods for working with contours.
     /// </summary>
-    public static class C
+    public static class ContourExtensions
+    {
+        /// <summary>
+        /// Extracts contours from all slices of the given volume, searching for the given foreground value.
+        /// Contour extraction will take holes into account.
+        /// </summary>
+        /// <param name="volume"></param>
+        /// <param name="foregroundId">The voxel value that should be used in the contour search as foreground.</param>
+        /// <param name="axialSmoothingType">The smoothing that should be applied when going from a point polygon to
+        /// contours. This will only affect axial slice, for other slice types no smoothing will be applied.
+        /// <param name="sliceType">The type of slice that should be used for contour extraction.</param>
+        /// <param name="filterEmptyContours">If true, contours with no points are not extracted.</param>
+        /// <param name="regionOfInterest"></param>
+        /// <returns></returns>
+        public static ContoursPerSlice ContoursWithHolesPerSlice(
+            this Volume3D<byte> volume,
+            byte foregroundId = ModelConstants.MaskForegroundIntensity,
+            SliceType sliceType = SliceType.Axial,
+            bool filterEmptyContours = true,
+            Region3D<int> regionOfInterest = null,
+            ContourSmoothingType axialSmoothingType = ContourSmoothingType.Small)
+ 
