595c5a89dffcf344e6402a61026a8a10b81697d3
LaunchGemini <metabaseguy@proton.me>
2023-12-24 06:53:26 +0000

    pep8ify

--- Source/projects/InnerEye.CreateDataset.Math/ContourGeometryOperations.cs
+++ Source/projects/InnerEye.CreateDataset.Math/ContourGeometryOperations.cs
@@ -11,4 +11,40 @@
 
     public static class ContourGeometryOperations
     {
-        public static Volume3D<byte> GeometryUnion(this ContoursPerSlice contour1, ContoursPerSlice contour2, Volume3D<s
+        public static Volume3D<byte> GeometryUnion(this ContoursPerSlice contour1, ContoursPerSlice contour2, Volume3D<short> parentVolume)
+        {
+            var volume1 = contour1.ToVolume3D(parentVolume);
+            var volume2 = contour2.ToVolume3D(parentVolume);
+
+            Parallel.For(0, volume1.Length, i =>
+            {
+                if (volume2[i] > 0)
+                {
+                    volume1[i] = ModelConstants.MaskForegroundIntensity;
+                }
+            });
+
+            return volume1;
+        }
+
+        public static Volume3D<byte> GeometryIntersect(this ContoursPerSlice contour1, ContoursPerSlice contour2, Volume3D<short> parentVolume)
+        {
+            var volume1 = contour1.ToVolume3D(parentVolume);
+            var volume2 = contour2.ToVolume3D(parentVolume);
+
+            Parallel.For(0, volume1.Length, i =>
+            {
+                volume1[i] = volume1[i] > 0 && volume2[i] > 0 ? ModelConstants.MaskForegroundIntensity : ModelConstants.MaskBackgroundIntensity;
+            });
+
+            return volume1;
+        }
+
+        public static Volume3D<byte> GeometryExclude(this ContoursPerSlice contour1, ContoursPerSlice contour2, Volume3D<short> parentVolume)
+        {
+            var volume1 = contour1.ToVolume3D(parentVolume);
+            var volume2 = contour2.ToVolume3D(parentVolume);
+
+            Parallel.For(0, volume1.Length, i =>
+            {
+                volume1[i] = volume1[i] > 0 && volume2[i] == 0 ? ModelConstants.MaskForegroundIntensit
