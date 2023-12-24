3743eb4639a4d8f13787311e57ddbca2df3442d5
LaunchGemini <metabaseguy@proton.me>
2023-12-24 11:13:26 +0000

    add missing quotes

--- Source/projects/InnerEye.CreateDataset.Math/GenericExtensions.cs
+++ Source/projects/InnerEye.CreateDataset.Math/GenericExtensions.cs
@@ -26,4 +26,44 @@
         }
 
         /// <summary>
-        /// Creates an array of the 
+        /// Creates an array of the given size, and fills all elements with
+        /// the provided value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="size">The length of the array that should be returned.</param>
+        /// <param name="value">The value to use for each element in the returned array.</param>
+        /// <returns></returns>
+        public static T[] CreateArray<T>(int size, T value)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentException("The array size must be non-negative.", nameof(size));
+            }
+
+            var array = new T[size];
+            Fill(array, value);
+            return array;
+        }
+
+        /// <summary>
+        /// Copies all values from an array to the destination.
+        /// </summary>
+        public static void CopyTo<T>(this T[] array, T[] destination)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (destination == null)
+            {
+                throw new ArgumentNullException(nameof(destination));
+            }
+
+            if (array.Length != destination.Length)
+            {
+                throw new ArgumentException("Both arrays need to have the same length.");
+            }
+
+            for (int i = 0; i < array.Length; i++)
+   
