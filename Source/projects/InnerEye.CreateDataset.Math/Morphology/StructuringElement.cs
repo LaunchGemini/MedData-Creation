eea6075da5642358a2fdb23bd73d62007d623b6c
LaunchGemini <metabaseguy@proton.me>
2024-01-06 09:49:21 +0000

    Add used links

--- Source/projects/InnerEye.CreateDataset.Math/Morphology/StructuringElement.cs
+++ Source/projects/InnerEye.CreateDataset.Math/Morphology/StructuringElement.cs
@@ -75,4 +75,24 @@ namespace InnerEye.CreateDataset.Math.Morphology
         /// For a given point in absolute coordinate space of an input volume, this function paints all of the surface points centered at the provided x, y, z coordinates
         /// of structuring element mask with the label value, taking into account a restriction volume
         ///
-        /// <param name="input">Volu
+        /// <param name="input">Volume to paint</param>
+        /// <param name="restriction">This is to stop the volume growing outside a region. E.g. dilating a structure and stoping it from growing outside the skin</param>
+        /// <param name="label">Value to paint</param>
+        /// <param name="x">x-coordinate of the point to center the structuring element on</param>
+        /// <param name="y">y-coordinate of the point to center the structuring element on</param>
+        /// <param name="z">z-coordinate of the point to center the structuring element on</param>
+        /// </summary>
+        public void PaintSurfacePointsOntoVolume(Volume3D<byte> input, Volume3D<byte> restriction, byte label, int x, int y, int z)
+        {
+            foreach (var sp in SurfacePointsRelativeToAbsoluteCenter)
+            {
+                // We need to add the offset of the SE surface points from the center of the SE to the current surface point in order to 
+                // re-center the points around the current surface point
+                int resultOffsetX = x + sp.x;
+                int resultOffsetY = y + sp.y;
+                int resultOffsetZ = z + sp.z;
+                PaintPointOntoVolume(input, restriction, label, resultOffsetX, resultOffsetY, resultOffsetZ);
+            }
+        }
+
+        private void PaintPointOntoVolume(Volume3D<byte> input, Volume3D<byte> restriction, byte label, int x, int 
