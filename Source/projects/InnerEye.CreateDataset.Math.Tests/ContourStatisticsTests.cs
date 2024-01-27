d34f7878acc2d6db4c74fe4cef845851cf7cc947
LaunchGemini <metabaseguy@proton.me>
2024-01-27 18:53:04 +0000

    unix: fix a race condition in unix_release()

--- Source/projects/InnerEye.CreateDataset.Math.Tests/ContourStatisticsTests.cs
+++ Source/projects/InnerEye.CreateDataset.Math.Tests/ContourStatisticsTests.cs
@@ -15,4 +15,51 @@
     [TestFixture]
     public class ContourStatisticsTests
     {
-        private Volume3D<T> CreateVolume<T>(T
+        private Volume3D<T> CreateVolume<T>(T[] array, int dimX, int dimY, int dimZ) =>
+            new Volume3D<T>(array, dimX, dimY, dimZ, 1, 1, 1, new Point3D(), Matrix3.CreateIdentity());
+
+        [Test()]
+        public void CheckBasicVolumeStats()
+        {
+            var volumeArray = new short[]
+            {
+                10, 10, 10,
+                10, 20, 0,
+                10, 0, 20
+            };
+
+            var volume = CreateVolume(volumeArray, 3, 3, 1);
+
+            var contourVolumeArray = new byte[]
+           {
+                0, 0, 0,
+                0, 1, 1,
+                0, 1, 1
+           };
+
+            var contourVolume = CreateVolume(contourVolumeArray, 3, 3, 1);
+
+            var stats = ContourStatistics.FromVolumeAndMask(new ReadOnlyVolume3D<short>(volume), contourVolume);
+
+            Assert.AreEqual(10, stats.VoxelValueMean);
+            Assert.AreEqual(10, stats.VoxelValueStandardDeviation);
+            Assert.AreEqual(0.004, stats.SizeInCubicCentimeters);
+        }
+
+        [Test()]
+        public void EmptyContour()
+        {
+            var volumeArray = new short[]
+            {
+                10, 10, 10,
+                10, 20, 0,
+                10, 0, 20
+            };
+
+            var volume = CreateVolume(volumeArray, 3, 3, 1);
+
+            var contourVolumeArray = new byte[]
+           {
+                0, 0, 0,
+                0, 0, 0,
+ 
