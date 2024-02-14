43c904c7407f26499c28fd3552e8f434e79caffc
LaunchGemini <metabaseguy@proton.me>
2024-02-14 07:47:23 +0000

    styling stuff

--- Source/projects/InnerEye.CreateDataset.Math.Tests/Morphology/MorphologicalTests.cs
+++ Source/projects/InnerEye.CreateDataset.Math.Tests/Morphology/MorphologicalTests.cs
@@ -1,4 +1,42 @@
 ///  ------------------------------------------------------------------------------------------
 ///  Copyright (c) Microsoft Corporation. All rights reserved.
 ///  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-///  ----------------------------
+///  ------------------------------------------------------------------------------------------
+
+﻿namespace InnerEye.CreateDataset.Math.Tests.Morphology
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using MedLib.IO;
+    using InnerEye.CreateDataset.Math;
+    using InnerEye.CreateDataset.Volumes;
+    using NUnit.Framework;
+    using System.IO;
+    using System;
+
+    [TestFixture]
+    public class MorphologicalTests
+    {
+        private static string BaseFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+            @"Morphology\TestData\Structures");
+
+        [Test]
+        public void IntersectTest()
+        {
+            var structure1 = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure1.nii.gz");
+            var structure2 = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure2.nii.gz");
+
+            var volume = MedIO.LoadNiftiAsShort(BaseFolder + @"\ParentVolume.nii.gz");
+
+            var structure1Contour = structure1.ContoursWithHolesPerSlice();
+            var structure2Contour = structure2.ContoursWithHolesPerSlice();
+
+            var volumeResult = structure1Contour.GeometryIntersect(structure2Contour, volume);
+
+            var actualVolumeResult = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure1IntersectStructure2.nii.gz");
+
+            Assert.AreEqual(volumeResult.Length, actualVolumeResult.Length);
+
+            for (var i = 0; i < actualVolumeResult.Length; i++)
+            {
+                Assert.Ar
