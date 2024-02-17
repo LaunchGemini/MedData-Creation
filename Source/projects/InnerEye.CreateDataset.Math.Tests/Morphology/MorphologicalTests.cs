1735aa26740eb8d4b9f207af940514916f7f99e7
LaunchGemini <metabaseguy@proton.me>
2024-02-17 08:55:59 +0000

    MPI: Fix compilation on MIPS with GCC 4.4 and newer

--- Source/projects/InnerEye.CreateDataset.Math.Tests/Morphology/MorphologicalTests.cs
+++ Source/projects/InnerEye.CreateDataset.Math.Tests/Morphology/MorphologicalTests.cs
@@ -39,4 +39,39 @@
 
             for (var i = 0; i < actualVolumeResult.Length; i++)
             {
-                Assert.Ar
+                Assert.AreEqual(volumeResult[i], actualVolumeResult[i]);
+            }
+        }
+
+        [Test]
+        public void MinusTest()
+        {
+            var structure1 = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure1.nii.gz");
+            var structure2 = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure2.nii.gz");
+
+            var volume = MedIO.LoadNiftiAsShort(BaseFolder + @"\ParentVolume.nii.gz");
+
+            var structure1Contour = structure1.ContoursWithHolesPerSlice();
+            var structure2Contour = structure2.ContoursWithHolesPerSlice();
+
+            var volumeResult = structure1Contour.GeometryExclude(structure2Contour, volume);
+
+            var actualVolumeResult = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure1MinusStructure2.nii.gz");
+
+            Assert.AreEqual(volumeResult.Length, actualVolumeResult.Length);
+
+            for (var i = 0; i < actualVolumeResult.Length; i++)
+            {
+                Assert.AreEqual(volumeResult[i], actualVolumeResult[i]);
+            }
+        }
+
+        [Test]
+        public void UnionTest()
+        {
+            var structure1 = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure1.nii.gz");
+            var structure2 = MedIO.LoadNiftiAsByte(BaseFolder + @"\Structure2.nii.gz");
+
+            var volume = MedIO.LoadNiftiAsShort(BaseFolder + @"\ParentVolume.nii.gz");
+
+            var structu
