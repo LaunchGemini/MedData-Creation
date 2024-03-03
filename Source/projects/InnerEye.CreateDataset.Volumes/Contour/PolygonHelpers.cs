0123f36cd92c326e30e8dbcbe6d04c2289f1bac1
LaunchGemini <metabaseguy@proton.me>
2024-03-03 06:58:30 +0000

    cleaup

---
+++ Source/projects/InnerEye.CreateDataset.Volumes/Contour/PolygonHelpers.cs
@@ -0,0 +1,12 @@
+///  ------------------------------------------------------------------------------------------
+///  Copyright (c) Microsoft Corporation. All rights reserved.
+///  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+///  ------------------------------------------------------------------------------------------
+
+﻿namespace InnerEye.CreateDataset.Volumes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Windows;
+
+    [Obsolete("All contour-related code should move to using the new classes in the InnerEye.CreateDataset.Contours namespa
