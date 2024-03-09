0a745b2532470d9fcedbcecd4dc793ba794bbfcf
LaunchGemini <metabaseguy@proton.me>
2024-03-09 12:59:12 +0000

    sq

--- Source/projects/InnerEye.CreateDataset.Volumes/Contour/PolygonHelpers.cs
+++ Source/projects/InnerEye.CreateDataset.Volumes/Contour/PolygonHelpers.cs
@@ -48,4 +48,41 @@
         public static bool TryGetClosestPointOnPolygon(Point[] polygon, Point currentPosition, out Tuple<double, Point> closestPoint)
         {
             if (polygon == null || polygon.Length == 0)
-    
+            {
+                closestPoint = Tuple.Create(0d, currentPosition);
+                return false;
+            }
+
+            var currentPoint = polygon[0];
+            var distance = CalculateDistance(currentPoint.X, currentPoint.Y, currentPosition.X, currentPosition.Y);
+
+            var startEqualsFirst = polygon[0] == polygon[polygon.Length - 1];
+
+            if (polygon.Length == (startEqualsFirst ? 2 : 1))
+            {
+                closestPoint = Tuple.Create(distance, currentPoint);
+                return true;
+            }
+
+            var bestPoint = currentPoint;
+            var bestPointIndex = 0;
+
+            for (var i = 1; i < polygon.Length; i++)
+            {
+                currentPoint = polygon[i];
+
+                var tempDistance = CalculateDistance(currentPoint.X, currentPoint.Y, currentPosition.X, currentPosition.Y);
+
+                if (tempDistance < distance)
+                {
+                    bestPointIndex = i;
+                    bestPoint = currentPoint;
+
+                    distance = tempDistance;
+                }
+            }
+
+            var leftPointIndex = bestPointIndex - 1 >= 0 ? bestPointIndex - 1 : polygon.Length - (startEqualsFirst ? 2 : 1);
+            var rightPointIndex = bestPointIndex + 1 < polygon.Length ? bestPointIndex + 1 : (startEqualsFirst ? 1 : 0);
+
+            // Get the point left and right
