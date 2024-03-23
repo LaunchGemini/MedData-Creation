19d4a96fdc91d729116f42d0308ba0adb37f4726
LaunchGemini <metabaseguy@proton.me>
2024-03-23 11:05:02 +0000

    USB: cdc-acm: fix write and suspend race

--- Source/projects/InnerEye.CreateDataset.Volumes/Matrix4.cs
+++ Source/projects/InnerEye.CreateDataset.Volumes/Matrix4.cs
@@ -23,4 +23,39 @@ namespace InnerEye.CreateDataset.Volumes
         public double this[int row, int column]
         {
             get { return Data[row + column * 4]; }
-       
+            set { Data[row + column * 4] = value; }
+        }
+        
+        public double[] Data { get; }
+
+        public void Zero()
+        {
+            for (var i = 0; i < 16; i++)
+            {
+                Data[i] = 0;
+            }
+        }
+
+        public void Identity()
+        {
+            for (var i = 0; i < 16; i++)
+            {
+                Data[i] = 0;
+            }
+
+            Data[0] = 1;
+            Data[5] = 1;
+            Data[10] = 1;
+            Data[15] = 1;
+        }
+        
+        public static Matrix4 operator *(Matrix4 a, Matrix4 b)
+        {
+            var result = new Matrix4();
+
+            for (var i = 0; i < 4; i++)
+            {
+                result.Data[i * 4 + 0] = a.Data[0] * b.Data[i * 4 + 0] + a.Data[4] * b.Data[i * 4 + 1] + a.Data[8] * b.Data[i * 4 + 2] + a.Data[12] * b.Data[i * 4 + 3];
+                result.Data[i * 4 + 1] = a.Data[1] * b.Data[i * 4 + 0] + a.Data[5] * b.Data[i * 4 + 1] + a.Data[9] * b.Data[i * 4 + 2] + a.Data[13] * b.Data[i * 4 + 3];
+                result.Data[i * 4 + 2] = a.Data[2] * b.Data[i * 4 + 0] + a.Data[6] * b.Data[i * 4 + 1] + a.Data[10] * b.Data[i * 4 + 2] + a.Data[14] * b.Data[i * 4 + 3];
+                result.Data[i * 4 + 3] = a.Data[3] * b.Data[i * 4 + 0] + a.Data[7] * b.Data[i * 4 + 1] + a.Data[11] * b.Data[i * 4 + 2] + a.D
