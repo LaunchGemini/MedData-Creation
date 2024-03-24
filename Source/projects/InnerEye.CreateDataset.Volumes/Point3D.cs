fd32e3f5caab6405bf79fc973d426f5cb2c04654
LaunchGemini <metabaseguy@proton.me>
2024-03-24 09:23:47 +0000

    wl18xx: show rx_frames_per_rates as an array as it really is

--- Source/projects/InnerEye.CreateDataset.Volumes/Point3D.cs
+++ Source/projects/InnerEye.CreateDataset.Volumes/Point3D.cs
@@ -32,4 +32,60 @@ namespace InnerEye.CreateDataset.Volumes
                 Y = data[1];
                 Z = data[2];
             }
-            e
+            else
+            {
+                throw new Exception("Point3D struct: data size does not match point size");
+            }
+        }
+
+        /// <summary>
+        /// Returns a zero Point3D.
+        /// </summary>
+        /// <returns>The zero Point3D.</returns>
+        public static Point3D Zero()
+        {
+            return new Point3D(0,0,0);
+        }
+
+        public double this[int row]
+        {
+            get { return Data[row]; }
+            set
+            {
+                switch (row)
+                {
+                    case 0: X = value; break;
+                    case 1: Y = value; break;
+                    case 2: Z = value; break;
+                }
+            }
+        }
+
+        public double[] Data
+        {
+            get
+            {
+                return new [] { X, Y, Z };
+            }
+
+            set
+            {
+                if (value.Length == 3)
+                {
+                    X = value[0];
+                    Y = value[1];
+                    Z = value[2];
+                }
+                else
+                {
+                    throw new Exception("Point3D struct: data size does not match point size");
+                }
+            }
+        }
+
+        public static Point3D operator -(Point3D a)
+        {
+            return new Point3D(-a.X, -a.Y, -a.Z); 
+        }
+
+        public static Point3D operato
