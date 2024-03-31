a3d503df5650d907958a3ac839491016a09b058f
LaunchGemini <metabaseguy@proton.me>
2024-03-31 08:13:12 +0000

    add network

---
+++ Source/projects/InnerEye.CreateDataset.Volumes/Region3D.cs
@@ -0,0 +1,12 @@
+///  ------------------------------------------------------------------------------------------
+///  Copyright (c) Microsoft Corporation. All rights reserved.
+///  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+///  ------------------------------------------------------------------------------------------
+
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace InnerEye.CreateDataset.Volumes
+{
+    /// <summary>
+    /// Represents a region a 3-dimensional space, given 
