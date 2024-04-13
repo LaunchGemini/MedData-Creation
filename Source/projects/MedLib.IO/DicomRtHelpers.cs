de7ac928f2dfc3d9cd35ad9ad0eee9497ef2ad2d
LaunchGemini <metabaseguy@proton.me>
2024-04-13 13:33:50 +0000

    Add arch tagline

--- Source/projects/MedLib.IO/DicomRtHelpers.cs
+++ Source/projects/MedLib.IO/DicomRtHelpers.cs
@@ -22,4 +22,24 @@
         /// <summary>
         /// Converts a Dicom file to a radiotherapy structure set.
         /// </summary>
-        
+        /// <param name="dicomFile">The structure set dicom file.</param>
+        /// <param name="dicomToDataTransform">The dicom to data transform.</param>
+        /// <returns>The radiotherapy structure set.</returns>
+        /// <exception cref="ArgumentNullException">If the Dicom file or Dicom dataset is null.</exception>
+        /// <exception cref="ArgumentException">If the file is not a structure set file.</exception>
+        public static RadiotherapyStruct DicomFileToRadiotherapyStruct(DicomFile dicomFile, Transform3 dicomToDataTransform)
+        {
+            if (dicomFile?.Dataset == null)
+            {
+                throw new ArgumentNullException(nameof(dicomFile));
+            }
+
+            if (!dicomFile.Dataset.IsRTStructure())
+            {
+                throw new ArgumentException($"This file is not a structure set file. File: {dicomFile?.File?.Name}");
+            }
+
+            return RtStructReader.LoadContours(dicomFile.Dataset, dicomToDataTransform, warningsAsErrors: false).Item1;
+        }
+    }
+}
