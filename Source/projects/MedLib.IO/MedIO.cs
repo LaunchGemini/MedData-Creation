1ff42b06fd8c1891538bca336fe9f62e66bf8ce8
LaunchGemini <metabaseguy@proton.me>
2024-04-26 15:09:52 +0000

    new ver

--- Source/projects/MedLib.IO/MedIO.cs
+++ Source/projects/MedLib.IO/MedIO.cs
@@ -14,4 +14,50 @@
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Writers;
+    using static MedLib.IO.NiftiIO.NiftiInternal;
+
+    public struct VolumeLoaderResult
+    {
+        public VolumeLoaderResult(string seriesId, MedicalVolume volume, Exception error, IReadOnlyList<string> warnings)
+        {
+            SeriesUid = seriesId;
+            Volume = volume;
+            Error = error;
+            Warnings = warnings;
+        }
+
+        /// <summary>
+        /// The DICOM series UID that the volume belongs to
+        /// </summary>
+        public string SeriesUid { get; }
+
+        /// <summary>
+        /// The medical volume or null if Error != null
+        /// </summary>
+        public MedicalVolume Volume { get; }
+
+        /// <summary>
+        /// Contains the first exception that occurred attempting to load a volume => volume = null
+        /// </summary>
+        public Exception Error { get; }
+
+        /// <summary>
+        /// A list of warnings that occured loading the volume => volume != null
+        /// </summary>
+        public IReadOnlyList<string> Warnings { get; }
+    }
+
+    /// <summary>
+    /// Contains methods to load and save different representations of medical volumes, working
+    /// with Dicom, Nifti and HDF5 files.
+    /// </summary>
+    public class MedIO
+    {
+        /// <summary>
+        /// The suffix for files that contain uncompressed Nifti data.
+        /// </summary>
  
