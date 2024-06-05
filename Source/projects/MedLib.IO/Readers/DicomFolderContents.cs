d24cf201f4f5ef9740207c7d41f34bd4df240029
LaunchGemini <metabaseguy@proton.me>
2024-06-05 14:51:01 +0000

    fixed a mistake

--- Source/projects/MedLib.IO/Readers/DicomFolderContents.cs
+++ Source/projects/MedLib.IO/Readers/DicomFolderContents.cs
@@ -20,3 +20,31 @@ namespace MedLib.IO.Readers
         /// <summary>
         /// The list of {SeriesUID, SOPInstance list} for recognized image types CT & MR in this folder 
         /// </summary>
+        public IReadOnlyList<DicomSeriesContent> Series { get; private set; }
+
+        /// <summary>
+        /// The list of {Referenced SeriesUID, SOPInstance list} for recognized RTStructs in this folder. 
+        /// </summary>
+        public IReadOnlyList<DicomSeriesContent> RTStructs { get; private set; }
+
+        /// <summary>
+        /// The folder path this instance was generated from.
+        /// </summary>
+        public string FolderPath { get; private set; }
+
+        public static DicomFolderContents Build(IReadOnlyList<DicomFileAndPath> fileAndPaths)
+        {
+            // Extract the RT structs and group by referenced SeriesUID
+            var rtStructs = fileAndPaths.Where((fp) => fp.File.Dataset.GetSingleValueOrDefault(DicomTag.SOPClassUID, DicomExtensions.EmptyUid) == DicomUID.RTStructureSetStorage);
+
+            // We assume there is 1 and only 1 referenced series
+            var parsedReferencedFoR = rtStructs.GroupBy(
+                (rt) => DicomRTStructureSet.Read(rt.File.Dataset).
+                    ReferencedFramesOfRef.FirstOrDefault()?.
+                    ReferencedStudies?.FirstOrDefault()?.
+                    ReferencedSeries.FirstOrDefault()?.
+                    SeriesInstanceUID ?? string.Empty
+            );
+
+            // Filter out the CT and MR SOPClasses
+            var ctMR = fileAndPaths.Where((fp
