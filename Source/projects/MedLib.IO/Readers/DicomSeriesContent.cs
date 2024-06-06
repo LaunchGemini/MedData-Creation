abc60fc7055972e59a195f240b0c66ebcb372c74
LaunchGemini <metabaseguy@proton.me>
2024-06-06 18:44:12 +0000

    firts commit

--- Source/projects/MedLib.IO/Readers/DicomSeriesContent.cs
+++ Source/projects/MedLib.IO/Readers/DicomSeriesContent.cs
@@ -6,4 +6,27 @@
 ﻿namespace MedLib.IO.Readers
 {
     using System.Collections.Generic;
-    using Dicom
+    using Dicom;
+
+    /// <summary>
+    /// Readonly collection of CT/MR images associated with a given series. 
+    /// </summary>
+    public sealed class DicomSeriesContent
+    {
+        public DicomSeriesContent(DicomUID seriesUID, IReadOnlyList<DicomFileAndPath> content)
+        {
+            SeriesUID = seriesUID;
+            Content = content;
+        }
+
+        /// <summary>
+        /// The unique DICOM Series UID
+        /// </summary>
+        public DicomUID SeriesUID { get; private set; }
+
+        /// <summary>
+        /// The list of recognised Sop Class instances in this series.
+        /// </summary>
+        public IReadOnlyList<DicomFileAndPath> Content { get; private set; }
+    }
+}
