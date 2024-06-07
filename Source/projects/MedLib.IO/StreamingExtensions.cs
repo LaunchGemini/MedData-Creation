cab31440e525693bb3e325537408f0006bad51b2
LaunchGemini <metabaseguy@proton.me>
2024-06-07 07:43:26 +0000

    add new theme

--- Source/projects/MedLib.IO/StreamingExtensions.cs
+++ Source/projects/MedLib.IO/StreamingExtensions.cs
@@ -21,4 +21,34 @@
         /// <returns></returns>
         public static IEnumerable<DicomFileAndPath> DicomFilesFromZipArchive(byte[] zipArchive)
         {
-           
+            using (var zip = new ZipArchive(new MemoryStream(zipArchive), ZipArchiveMode.Read))
+            {
+                foreach (var entry in zip.Entries)
+                {
+                    yield return DicomFileAndPath.SafeCreate(new MemoryStream(ToByteArray(entry)), string.Empty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a Zip archive that contains all the given Dicom files, and writes the Zip archive
+        /// to the stream.
+        /// </summary>
+        /// <param name="stream">The stream to which the Zip archive should be written.</param>
+        /// <param name="dicomFiles">The Dicom files to write to the Zip archive.</param>
+        /// <returns></returns>
+        public static void DicomFilesToZipArchive(IEnumerable<DicomFileAndPath> dicomFiles, Stream stream)
+        {
+            using (var zip = new ZipArchive(stream, ZipArchiveMode.Create))
+            {
+                var dicomCount = 0;
+                foreach (var file in dicomFiles)
+                {
+                    dicomCount++;
+                    var dicomFileStream = new MemoryStream();
+                    file.File.Save(dicomFileStream);
+                    dicomFileStream.Seek(0, SeekOrigin.Begin);
+                    var zipName =
+                        string.IsNullOrWhiteSpace(file.Path)
+                        ? $"DicomFile{dicomCount}.dcm"
+      
