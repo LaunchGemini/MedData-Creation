a9e7c2934b3b7a503dbc1d13f2270092333d6aff
LaunchGemini <metabaseguy@proton.me>
2024-06-09 16:14:27 +0000

    Update Debian_package.md

--- Source/projects/MedLib.IO.Tests/DicomDatsetExtensionsTests.cs
+++ Source/projects/MedLib.IO.Tests/DicomDatsetExtensionsTests.cs
@@ -15,4 +15,38 @@
 
     using InnerEye.CreateDataset.Volumes;
 
-    using NU
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DicomDatsetExtensionsTests
+    {
+        [Description("Tests all the Dicom dataset extensions.")]
+        [Test]
+        public async Task TestAllExtensions()
+        {
+            var prostateFolder = TestData.GetFullImagesPath("sample_dicom");
+            var files = Directory.EnumerateFiles(prostateFolder).ToList();
+
+            var dicomFiles = new DicomFile[files.Count];
+
+            for (var i = 0; i < files.Count; i++)
+            {
+                dicomFiles[i] = await DicomFile.OpenAsync(files[i]);
+            }
+
+            var (width, height) = dicomFiles[0].Dataset.GetSliceSize();
+            var origin = dicomFiles[0].Dataset.GetOrigin();
+            var (spacingX, spacingY) = dicomFiles[0].Dataset.GetPixelSpacings();
+            var direction = dicomFiles[0].Dataset.GetDirectionalMatrix();
+            var rescaleIntercept = dicomFiles[0].Dataset.GetRescaleIntercept();
+            var rescaleSlope = dicomFiles[0].Dataset.GetRescaleSlope();
+            var isSignedPixelRepresentation = dicomFiles[0].Dataset.IsSignedPixelRepresentation();
+            var highBit = dicomFiles[0].Dataset.GetHighBit();
+            var sopClass = dicomFiles[0].Dataset.GetSopClass();
+
+            Assert.AreEqual(512, width);
+            Assert.AreEqual(512, height);
+
+            Assert.AreEqual(-250, origin.X);
+            Assert.AreEqual(-250, origin.Y);
+      
